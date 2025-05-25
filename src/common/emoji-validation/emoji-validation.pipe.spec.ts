58880781d429510e7872b761dd6157e9a2f16f79
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-05-25 17:15:44 +0100

    adding test for emoji

--- src/common/emoji-validation/emoji-validation.pipe.spec.ts
+++ src/common/emoji-validation/emoji-validation.pipe.spec.ts
@@ -1,7 +1,12 @@
 import { EmojiValidationPipe } from './emoji-validation.pipe';
 
 describe('EmojiValidationPipe', () => {
+  const emojiPipe = new EmojiValidationPipe().transform(undefined);
   it('should be defined', () => {
     expect(new EmojiValidationPipe()).toBeDefined();
+  });
+
+  it('should be undefined if no value is passed in', () => {
+    expect(emojiPipe).toBeUndefined();
   });
 });
--- src/common/emoji-validation/emoji-validation.pipe.ts
+++ src/common/emoji-validation/emoji-validation.pipe.ts
@@ -3,7 +3,8 @@ import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 @Injectable()
 export class EmojiValidationPipe implements PipeTransform {
   transform(value: any) {
-    console.log('LIFECYCLE PIPE');
+    console.log('LIFECYCLE 4: PIPE');
+    console.log(value);
     if (!value) return;
 
     if (value < 0 || value > 10)
