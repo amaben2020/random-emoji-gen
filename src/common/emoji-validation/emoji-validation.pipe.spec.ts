ee8b9636a0f53338eb4d5ef0641928d9929fbb73
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-05-25 17:21:27 +0100

    adding test cases

--- src/common/emoji-validation/emoji-validation.pipe.spec.ts
+++ src/common/emoji-validation/emoji-validation.pipe.spec.ts
@@ -1,3 +1,4 @@
+import { BadRequestException } from '@nestjs/common';
 import { EmojiValidationPipe } from './emoji-validation.pipe';
 
 describe('EmojiValidationPipe', () => {
@@ -8,5 +9,11 @@ describe('EmojiValidationPipe', () => {
 
   it('should be undefined if no value is passed in', () => {
     expect(emojiPipe).toBeUndefined();
+  });
+
+  it('should return a random emoji if the value is not an integer', () => {
+    expect(() => new EmojiValidationPipe().transform('amala')).toThrow(
+      BadRequestException,
+    );
   });
 });
