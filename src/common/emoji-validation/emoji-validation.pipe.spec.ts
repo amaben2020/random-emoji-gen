47f1ca19934ed8a58a1344c862878643abf97eef
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-05-26 08:21:30 +0100

    updating validation pipe

--- src/common/emoji-validation/emoji-validation.pipe.spec.ts
+++ src/common/emoji-validation/emoji-validation.pipe.spec.ts
@@ -1,19 +1,26 @@
 import { BadRequestException } from '@nestjs/common';
 import { EmojiValidationPipe } from './emoji-validation.pipe';
 
+const emojiValidationPipe = (value: string | number | null | undefined) =>
+  new EmojiValidationPipe().transform(value);
+
 describe('EmojiValidationPipe', () => {
-  const emojiPipe = new EmojiValidationPipe().transform(undefined);
   it('should be defined', () => {
     expect(new EmojiValidationPipe()).toBeDefined();
   });
 
   it('should be undefined if no value is passed in', () => {
-    expect(emojiPipe).toBeUndefined();
+    expect(emojiValidationPipe(undefined)).toBeUndefined();
   });
 
   it('should return a random emoji if the value is not an integer', () => {
-    expect(() => new EmojiValidationPipe().transform('amala')).toThrow(
-      BadRequestException,
-    );
+    expect(() => emojiValidationPipe('amala')).toThrow(BadRequestException);
+  });
+  it('should throw error if the value is less than zero', () => {
+    expect(() => emojiValidationPipe(-5)).toThrow(BadRequestException);
+  });
+
+  it('should return number for integer string', () => {
+    expect(emojiValidationPipe('5')).toBe(5);
   });
 });
--- src/common/emoji-validation/emoji-validation.pipe.ts
+++ src/common/emoji-validation/emoji-validation.pipe.ts
@@ -4,7 +4,7 @@ import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 export class EmojiValidationPipe implements PipeTransform {
   transform(value: any) {
     console.log('LIFECYCLE 4: PIPE');
-    console.log(value);
+
     if (!value) return;
 
     if (value < 0 || value > 10)
