03cf657b510f0cb218aa72fe99c434fe4dafe107
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-05-31 19:26:41 +0100

    creating create user dto

---
+++ src/users/users-create.dto.ts
@@ -0,0 +1,36 @@
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+  IsEmail,
+  Matches,
+} from 'class-validator';
+
+export class UsersCreateDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(90)
+  firstName: string;
+
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(90)
+  lastName: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
+  password: string;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,16 @@
 import {
+  Body,
   Controller,
   DefaultValuePipe,
   Get,
   Headers,
   Param,
+  ParseBoolPipe,
   ParseIntPipe,
+  Post,
   Query,
 } from '@nestjs/common';
+import { UsersCreateDto } from './users-create.dto';
 
 @Controller('users')
 export class UsersController {
@@ -16,12 +20,13 @@ export class UsersController {
     @Param('id', ParseIntPipe) id: string,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
     @Query('offset', new DefaultValuePipe(1), ParseIntPipe) offset: number,
+    @Query('available', ParseBoolPipe) available: boolean,
   ) {
-    return `id: ${id}, limit: ${limit}, offset: ${offset}`;
+    return `id: ${id}, limit: ${limit}, offset: ${offset} available: ${available}`;
   }
 
-  @Get()
-  findAll(): string[] {
-    return ['123'];
+  @Post()
+  createUser(@Body() request: UsersCreateDto) {
+    return `${request.email} ${request.firstName} ${request.lastName} ${request.password} has been created!`;
   }
 }
