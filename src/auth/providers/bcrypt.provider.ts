0d43ccc40f1c190b440b36726f2009a4e67604df
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-06-07 18:57:03 +0100

    bcryptjs upgrade

--- src/auth/providers/bcrypt.provider.ts
+++ src/auth/providers/bcrypt.provider.ts
@@ -1,20 +1,20 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { HashingProvider } from './hashing.provider';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class BcryptProvider implements HashingProvider {
   async hashPassword(password: string | Buffer): Promise<string> {
     const salt = await bcrypt.genSalt();
 
-    return bcrypt.hash(password, salt);
+    return bcrypt.hash(password as string, salt);
   }
 
   async comparePasswords(
     password: string | Buffer,
     hashedPassword: string,
   ): Promise<boolean> {
-    const isValid = await bcrypt.compare(password, hashedPassword);
+    const isValid = await bcrypt.compare(password as string, hashedPassword);
     return isValid;
   }
 }
